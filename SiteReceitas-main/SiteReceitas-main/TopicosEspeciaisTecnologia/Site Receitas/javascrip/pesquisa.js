1c049b97fbd37af959f9d3435750e63d49a3038d
Daniel Rocha <161393763+DannKes1@users.noreply.github.com>
2024-08-23 01:02:11 -0400

    Trabalho finalizado

--- SiteReceitas-main/SiteReceitas-main/TopicosEspeciaisTecnologia/Site Receitas/javascrip/pesquisa.js
+++ SiteReceitas-main/SiteReceitas-main/TopicosEspeciaisTecnologia/Site Receitas/javascrip/pesquisa.js
@@ -1,10 +1,20 @@
 const recipes = {
   "bolo de chocolate":
     "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/receitasHome.html/receita02.html",
-  "torta de limão":
+  "torta de maçã verde":
     "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/receitasHome.html/receita01.html",
   "receitas de doces":
     "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/html/doces.html",
+  "torta de maçã":
+    "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/receitasHome.html/receita01.html",
+  "pudim de chocolate":
+    "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/receitasHome.html/receita03.html",
+  "receitas de salgados":
+    "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/html/salgados.html",
+  "pão de batata doce":
+    "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/receitasHome.html/receita04.html",
+  "pao de batata doce":
+    "http://127.0.0.1:5500/SiteReceitas-main/TopicosEspeciaisTecnologia/Site%20Receitas/receitasHome.html/receita04.html",
 };
 
 function searchRecipes() {
